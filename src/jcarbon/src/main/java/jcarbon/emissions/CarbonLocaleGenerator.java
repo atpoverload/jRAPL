80d47271794615ec2b9838f63988b9cce3430211
jlee5205 <jlee5205@gmail.com>
2024-05-25 19:51:16 -0400

    added locale generator

--- src/jcarbon/src/main/java/jcarbon/emissions/CarbonLocaleGenerator.java
+++ src/jcarbon/src/main/java/jcarbon/emissions/CarbonLocaleGenerator.java
@@ -5,6 +5,9 @@ import static jcarbon.util.LoggerUtil.getLogger;
 
 
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -18,7 +21,11 @@ public final class CarbonLocaleGenerator{
     private static final Logger logger = getLogger();
     private static final ArrayList<String> ENUMS_LIST = getEnumsList();
     private static final ArrayList<String> CARBON_LOCALE_CLASS = buildClass();
-    private static final String CLASS_HEADER = "package jcarbon.emissions; public enum CarbonLocale {";
+    private static final String CLASS_HEADER = "package jcarbon.emissions; import java.util.Locale; public enum CarbonLocale {";
+    private static final String CONSTRUCTOR_BODY = "CarbonLocale(String locale, double intensity){ this.locale = locale; this.intensity = intensity; }";
+    private static final String ATTRIBUTES = "private final String locale; private final double intensity;";
+    private static final String FROM_LOCALE_HELPER = "public static CarbonLocale fromLocale(Locale localeCountry) { return CarbonLocale.valueOf(localeCountry.getISO3Country()); } ";
+    private static final String GET_DEFAULT_HELPER = "public static CarbonLocale getDefault() { return CarbonLocale.fromLocale(Locale.getDefault()); }";
 
     private static ArrayList<String> getEnumsList(){
         String filePath = System.getProperty("jcarbon.emissions.intensity");
@@ -52,6 +59,11 @@ public final class CarbonLocaleGenerator{
         ArrayList<String> builder = new ArrayList();
         builder.add(CLASS_HEADER);
         builder.addAll(ENUMS_LIST);
+        builder.add(ATTRIBUTES);
+        builder.add(CONSTRUCTOR_BODY);
+        builder.add(FROM_LOCALE_HELPER);
+        builder.add(GET_DEFAULT_HELPER);
+        builder.add("}");
         return builder;
     }
     /** Parses a csv file with a header like "locale,name,intensity". */
@@ -60,12 +72,29 @@ public final class CarbonLocaleGenerator{
         lines.stream().skip(1).map(s -> s.split(",")).forEach(token -> {
                 enums.add(String.format("%s(\"%s\", %f),",token[0], token[1], Double.parseDouble(token[2])));
         });
+        String last =  enums.get(enums.size() - 1).substring(0, enums.get(enums.size() - 1).length() - 1) + ";";
+        enums.set(enums.size() - 1, last);
         return enums;
     }
     
+    private static void toClass(){
+        try{
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("src/jcarbon/src/main/java/jcarbon/emissions/CarbonLocale.java")));
+            for(String e : CARBON_LOCALE_CLASS){
+                pw.write(String.format("%s\n", e));
+            }
+            pw.close();
+            System.out.println("Written successfully");
+        }
+        catch (IOException e) {
+            throw new IllegalStateException("Unable to write to default intensity file.", e);
+        }
+    }
+
     public static void main(String[] args){
         for (String e : CARBON_LOCALE_CLASS){
             System.out.println(e);
         }
+        toClass();
     }
 }
